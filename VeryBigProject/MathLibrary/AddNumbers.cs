d3507a91af61806b7218d812b7ddc227a40f4f74
Andrei Ignat <ignatandrei@yahoo.com>
2013-08-17 18:28:05 +0300

    first version

---
+++ VeryBigProject/MathLibrary/AddNumbers.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MathLibrary
+{
+    public class AddNumbers
+    {
+        public long Add(int i, int j, int k)
+        {
+            return (long)i + (long)j + (long)k; 
+        }
+    }
+}
---
+++ VeryBigProject/VeryBigProjectDOS/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using EncryptionLibrary;
+using MathLibrary;
+
+namespace VeryBigProjectDOS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var a=new AddNumbers();
+            var x= a.Add(1970,4,16);
+            var en = new EncryptString();
+            var c = en.Encrypt("http://msprogrammer.serviciipeweb.ro/");
+            Console.WriteLine(en.Encrypt(c));
+
+            var thisAssembly = Assembly.GetExecutingAssembly();
+            string pathAss = Path.GetDirectoryName(thisAssembly.Location);
+            var assemblies = thisAssembly.GetReferencedAssemblies();
+            foreach (var assembly in assemblies)
+            {
+                
+                var ass = Assembly.Load(assembly.FullName);
+
+                if (Path.GetDirectoryName(ass.Location) != pathAss)
+                    continue;
+
+                Console.WriteLine("-------------------");
+                Console.WriteLine(assembly.Name + ":" + assembly.Version);
+                
+                //Console.WriteLine(assembly.Name + assembly.Version);
+            }
+
+        }
+    }
+}
